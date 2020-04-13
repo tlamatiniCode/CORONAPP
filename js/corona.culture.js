545f1e635bc84e184c9e31f4eb1589854fe6d799
Alain Forton <495365+alainforton@users.noreply.github.com>
2020-04-13 20:43:22 +0200

    Update corona.culture.js

--- js/corona.culture.js
+++ js/corona.culture.js
@@ -1,12 +1,7 @@
 (function(){	
 	//input control	
 	this.regEx = /[0-9]{4,8}\d/;	
-	
-	//span icon
-	this.notest = "&#9785;";
-	this.waiting = "&#8986;";
-	this.tested = "&#10004;";
-	
+		
 	//texto result
 	this.notesteado = "NO TEST";
 	this.esperando = "ESPERANDO";
