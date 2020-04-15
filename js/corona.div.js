22cabfe88ad06864f9957ec228db479892a6fc17
Alain Forton <495365+alainforton@users.noreply.github.com>
2020-04-15 02:27:13 +0200

    Update corona.div.js

--- js/corona.div.js
+++ js/corona.div.js
@@ -1,7 +1,31 @@
 
 //functions enseñar estado
-function showTest(user){
-	var textodiv = user === undefined ? notesteado : user.texto;
-	 return divTest.innerHTML = `<span class="spanicon">${textodiv}</span>`;
-}
+function showTest(usertext){
+	this.user = usertext;
+	var textodiv = user == undefined ? notesteado : usertext.texto;
+	return divTest.innerHTML = `<span class="spanicon">${textodiv}</span>`;
+}	
+
+function showDiv(orNot,esMedico){
+	var textodiv = "";
 	
+	if(orNot == undefined){
+		for(var i = 0; i < prioridades.length ; i++){
+			textodiv += `<div class="selecdiv divsin" value="${i+1}">${prioridades[i]}</div>`;
+		}
+	}else if(orNot === esperando){
+		if(esMedico){
+			for(var i = 0; i < estado.length ; i++){
+				textodiv += `<div class="divestatus divsin" value="${i+1}">${estado[i]}</div>`;
+			}
+			return contenido.innerHTML = textodiv;
+		}
+		textodiv = `<div class="time" style="padding: 25px;">${textoespera} ${new Date()}</div>`;
+	}else if(orNot === testeado){
+		if(esMedico){
+			return contenido.innerHTML = `<div class="cambiardiv" style="display:block">${boton}</div>`;
+		}
+		textodiv = "";
+	}
+	return contenido.innerHTML = textodiv;
+}
