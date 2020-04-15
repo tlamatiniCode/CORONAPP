8106d41883ec828c21a87beb365c50e46ec2dd0e
Alain Forton <495365+alainforton@users.noreply.github.com>
2020-04-15 19:01:35 +0200

    Update corona.culture.js

--- js/corona.culture.js
+++ js/corona.culture.js
@@ -1,30 +1,30 @@
 (function(){	
 	//input control	
-	this.regEx 			  = /[0-9]{4,8}\d/;	
+        this.regEx = /[0-9]{4,8}\d/;	
 	
 	//literales
-	this.statusTest 	  = ["NO testeado",
-							"ID no válido.",
-							"SOLICITANDO",
-							"SOLICITADO",
-							"TESTEADO"]
+	this.statusTest = ["NO testeado",
+			"ID no válido.",
+			"SOLICITANDO",
+			"SOLICITADO",
+			"TESTEADO"]
 
 	this.statusResultTest =	["RECUPERADO",
-							"POSITIVO",
-							"NEGATIVO"];
+			"POSITIVO",
+			"NEGATIVO"];
 	
-	this.boton 			  = ["CONFIRMAR",
-							"MEDICO"];
+	this.boton = ["CONFIRMAR",
+			"MEDICO"];
 					
-	this.textos 		  = ["Te gustaría solicitar un tests?",
-							"Elige tu prioridad",
-							"En breve le avisaremos.<br>Se solicitó el : ",
-							"Se ha realizado el test quiere camibar el estado ?",
-							"Resultado del test : ",
-							"Su estado actual es : "];
+	this.textos = ["Te gustaría solicitar un tests?",
+			"Elige tu prioridad",
+			"En breve le avisaremos.<br>Se solicitó el : ",
+			"Se ha realizado el test quiere camibar el estado ?",
+			"Resultado del test : ",
+			"Su estado actual es : "];
 	
-	this.prioridades 	  = ["TRABAJO - necesita salir.",
-							"ESTUDIANTE - terminar el curso.", 
-							"OTROS - pensionistas, etc."];
+	this.prioridades = ["TRABAJO - necesita salir.",
+			"ESTUDIANTE - terminar el curso.", 
+			"OTROS - pensionistas, etc."];
 
 })();
