0512a0024dac2b6f95f560d1f515961fd00716fa
Alain Forton <495365+alainforton@users.noreply.github.com>
2020-04-15 02:26:47 +0200

    Update corona.culture.js

--- js/corona.culture.js
+++ js/corona.culture.js
@@ -1,9 +1,15 @@
 (function(){	
 	//input control	
 	this.regEx = /[0-9]{4,8}\d/;	
-		
 	//texto result
 	this.notesteado = "NO TEST";
 	this.esperando = "ESPERANDO";
 	this.testeado = "TESTEADO";
+	
+	this.estado = ["RECUPERADO","POSITIVO","NEGATIVO"];
+	
+	this.boton = "CAMBIAR";
+	this.textoespera = "Su solicitud se esta procesando.";
+	
+	this.prioridades = ["TRABAJO - necesita salir.","ESTUDIANTE - terminar el curso.", "TELETRABAJO - puede esperar.", "OTROS - pensionistas, etc."]
 })();
