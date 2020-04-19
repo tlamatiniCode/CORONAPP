e9ac5e05b4a9c5c7e8a69b0b107fd3958c867904
Alain Forton <495365+alainforton@users.noreply.github.com>
2020-04-19 14:22:21 +0200

    Update corona.div.js

--- js/corona.div.js
+++ js/corona.div.js
@@ -4,22 +4,21 @@ function showMe(spanTexto, divTexto, esMedico){
 	
 	//Si key borramos por defecto.
 	var textoDiv = "";
-	var textoSpan = spanFormatter(spanTexto);
-	doctor.classList.remove("selectoptactivo");
+	var textoSpan = spanFormatter(spanTexto);	
 	//Si no esta testeado
 	if(spanTexto == statusTest[0]){
-		
+		doctor.classList.remove("selectoptactivo");
 		textoDiv = '<div class="textos">'+divTexto+'</div><div class="confirmar">'+boton[0]+'</div>';
 	//Confirmar solicitar	
 	}else if(spanTexto == statusTest[2]){
-		
+		doctor.classList.remove("selectoptactivo");
 		textoDiv = '<div class="textos">'+divTexto+'</div>';
 		for(var i = 0; i < prioridades.length ; i++){
 			textoDiv += '<div class="selecdiv divsin" value="'+i+'">'+prioridades[i]+'</div>';
 		}		
 	//Solicitado medico
 	}else if(spanTexto == statusTest[3] && esMedico){
-		
+		doctor.classList.remove("selectoptactivo");
 		textoDiv = '<div class="textos">'+divTexto+'</div><div class="confirmar">'+boton[0]+'</div>';
 	//Solicitado muestra fechas
 	}else if(spanTexto == statusTest[3]){
@@ -28,7 +27,7 @@ function showMe(spanTexto, divTexto, esMedico){
 		doctor.classList.add("selectoptactivo");		
 	//Confirmar estado
 	}else if(spanTexto == statusTest[4] && esMedico){
-		
+		doctor.classList.remove("selectoptactivo");
 		textoDiv = '<div class="textos">'+divTexto+'</div>';
 		for(var i = 0; i < statusResultTest.length ; i++){
 			textoDiv += '<div class="divestatus divsin" value="'+i+'">'+statusResultTest[i]+'</div>';
@@ -39,11 +38,28 @@ function showMe(spanTexto, divTexto, esMedico){
 		textoDiv = '<div class="textos">'+divTexto+'<br>'+user.resultado+'</div>';
 		doctor.classList.add("selectoptactivo");
 	//Forzar borrado general.
-	}else if(spanTexto == undefined){		
+	}else if(spanTexto == undefined){
+		doctor.classList.remove("selectoptactivo");
 		nuevoUser.classList.remove("activado");
 		inputid.value = "";
 		var textoSpan = "";		
-	}	
+	//Idioma
+	}else if(spanTexto == statusTest[5]){
+		if(buttonCul.classList.contains("culturestyle")){
+			buttonCul.classList.replace("culturestyle","culturestyleactive");
+			this.guardaContenido = contenido.innerHTML;
+			this.guardaDiv = divTest.innerHTML;
+			for(var i = 0; i < buttonLang.length; i++){
+				textoDiv += '<div class="culture divsin" value="'+i+'">'+buttonLang[i]+'</div>';
+			}
+		}else if(buttonCul.classList.contains("culturestyleactive")){
+			buttonCul.classList.replace("culturestyleactive","culturestyle");
+			divTest.innerHTML = guardaDiv;
+			contenido.innerHTML = guardaContenido;
+			return;
+		}
+	//Mail - bugs	
+	}
 	divTest.innerHTML = textoSpan;
 	contenido.innerHTML = textoDiv;
 }
@@ -52,14 +68,14 @@ function showMe(spanTexto, divTexto, esMedico){
 function spanFormatter(param){
 	return '<span class="spanicon">'+param+'</span>';
 }
-//cambiar estado click y devolver valor seleccionado
-function getVal(div){
-	var hazreplace = div.classList[1] == "divsin"
-		? div.classList.replace("divsin","divcheck")
-		: div.classList.replace("divcheck","divsin");
+//Devolver valor seleccionado
+function getVal(div){	
 	return div.attributes.value.value;
 }
 function getDate(){
 	var d = new Date();
 	return d.toDateString();
 }
+
+
+
